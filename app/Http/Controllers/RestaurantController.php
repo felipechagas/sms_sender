bf4806c47f5e52d6d51e176f6827315251620fcd
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-09 20:47:23 -0300

    :construction: Starting restaurant CRUD functions

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Restaurant;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Create a new RestaurantController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of restaurants
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Create one new restaurant
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Obtains and show one restaurant
+     * @return Illuminate\Http\Response
+     */
+    public function show($restaurant)
+    {
+        
+    }
+
+    /**
+     * Update an existing restaurant
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $restaurant)
+    {
+        
+    }
+
+    /**
+     * Remove an existing restaurant
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($restaurant)
+    {
+        
+    }
+}
