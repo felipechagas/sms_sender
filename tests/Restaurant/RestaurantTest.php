6c58d76dc1bd375c3c559f5a7be6dfd78c4090b1
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-10 13:06:54 -0300

    :construction: Starting tests

---
+++ tests/Restaurant/RestaurantTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class RestaurantTest extends TestCase
+{
+    /**
+     * /restaurants [GET]
+     */
+    public function testShouldReturnAllRestaurants(){
+
+    }
+
+    /**
+     * /restaurants/id [GET]
+     */
+    public function testShouldReturnRestaurant(){
+
+    }
+
+    /**
+     * /restaurants [POST]
+     */
+    public function testShouldCreateRestaurant(){
+
+    }
+
+    /**
+     * /restaurants/id [PUT]
+     */
+    public function testShouldUpdateRestaurant(){
+
+    }
+
+    /**
+     * /restaurants/id [DELETE]
+     */
+    public function testShouldDeleteRestaurant(){
+
+    }
+
+}
