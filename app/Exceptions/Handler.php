d7445d6486b13c9dc851b7d7d58df6127991cf8e
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-10 00:48:48 -0300

    :construction: Adding generic exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,6 +47,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        return $this->errorResponse('Unexpected error. Try later', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
