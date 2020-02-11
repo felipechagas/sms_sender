f57aeeaea9a890d015ec3936c3fba848a7047e29
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-11 19:11:45 -0300

    :construction: Adding twilio credenciais to service

--- app/Services/SmsServices.php
+++ app/Services/SmsServices.php
@@ -6,6 +6,10 @@ use App\Traits\ApiResponser;
 
 class SmsService implements SmsServiceInterface
 {
+    private $account_sid;
+    private $auth_token;
+    private $twilio_number;
+
     use ApiResponser;
 
     /**
@@ -15,6 +19,9 @@ class SmsService implements SmsServiceInterface
      */
     public function __construct()
     {
+        $this->account_sid = getenv("TWILIO_SID");
+        $this->auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $this->twilio_number = getenv("TWILIO_NUMBER");
     }
 
     /**
