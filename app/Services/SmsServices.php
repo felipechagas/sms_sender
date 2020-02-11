57488d13f69af3f4d45f529030e99324b3261e04
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-11 18:38:38 -0300

    :construction: Creating sms service

---
+++ app/Services/SmsServices.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+class SmsService implements SmsServiceInterface
+{
+
+    /**
+     * Create a new SmsController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     *
+     *
+     * @param
+     * @return
+     */
+    public function send()
+    {
+    }
+}
