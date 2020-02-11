17bf5253fc369de631625e91c0efd2cf24848f9e
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-11 11:01:22 -0300

    :white_check_mark: Testing if route exists

---
+++ tests/Sms/SmsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Http\Response;
+
+class SmsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * /restaurants [GET]
+     * 200
+     */
+    public function testShouldReturnAllRestaurants()
+    {
+        $this->post("/sms/send", []);
+        $this->seeStatusCode(Response::HTTP_OK);
+    }
+}
