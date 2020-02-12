93ab627003671659ed21d9759b9aac7f1e3a4a9e
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-12 14:13:41 -0300

    :construction: Adding logic to send sms

--- app/Services/SmsServices.php
+++ app/Services/SmsServices.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Traits\ApiResponser;
+use App\Traits\Formatter;
 use App\Http\Controllers\RestaurantController;
+use Twilio\Rest\Client;
 
 class SmsService implements SmsServiceInterface
 {
@@ -13,6 +15,7 @@ class SmsService implements SmsServiceInterface
     private $restaurant;
 
     use ApiResponser;
+    use Formatter;
 
     /**
      * Create a new SmsController instance.
@@ -37,6 +40,19 @@ class SmsService implements SmsServiceInterface
     {
         $json_restaurant = $this->restaurant->show($restaurant_id);
         $restaurant = $json_restaurant->getData(true);
+
+        $restaurant_name = $restaurant['data']['name'];
+        $delivery_time = $this->timeFormatter($restaurant['data']['delivery_time']);
+
+        $client = new Client($this->account_sid, $this->auth_token);
+        $client->messages->create(
+            $phone_number,
+            array(
+                'from' => $this->twilio_number,
+                'body' => "Take Away: Your order on $restaurant_name was received." .
+                    "The estimated delivery time is $delivery_time."
+            )
+        );
 
         return $this->successResponse(array('status' => 'sent'));
     }
