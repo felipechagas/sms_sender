f1061cd4bb0df5442fb43218f56c5c7ae3a9466a
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-12 14:10:31 -0300

    :sparkles: Implement a trait to format delivery time

--- app/Traits/Formatter.php
+++ app/Traits/Formatter.php
@@ -4,4 +4,37 @@ namespace App\Traits;
 
 trait Formatter
 {
+    /**
+     * Formats time to hours and minutes
+     * @param  int $seconds
+     * @return string
+     */
+    private function timeFormatter($seconds)
+    {
+        $hours = floor($seconds / 3600);
+        $minutes = floor(($seconds / 60) % 60);
+        $minutes = (round($minutes) % 10 === 0) ? round($minutes) :
+            round(($minutes + 10 / 2) / 10) * 10;
+
+        if ($minutes == 60) {
+            $hours = $hours + 1;
+            $minutes = 0;
+        }
+
+        $hour_text = $hours == 1 ? 'hour' : 'hours';
+
+        if ($hours > 0 && $minutes > 0) {
+            return "$hours $hour_text, $minutes minutes";
+        }
+
+        if ($hours > 0 && $minutes == 0) {
+            return "$hours $hour_text";
+        }
+
+        if ($hours == 0 && $minutes > 0) {
+            return "$minutes minutes";
+        }
+
+        return "10 minutes";
+    }
 }
