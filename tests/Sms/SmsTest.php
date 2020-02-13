1974cb04a13b272b85e26b1842c5ac744a0fdd35
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-13 20:38:23 -0300

    :white_check_mark: Refactoring unit tests for sms sender

--- tests/Sms/SmsTest.php
+++ tests/Sms/SmsTest.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\RestaurantController;
 use App\Restaurant;
+use App\Message;
 use App\Services\SmsService;
 use App\Traits\ApiResponser;
 use Laravel\Lumen\Testing\DatabaseTransactions;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Twilio\Rest\Client;
 use \Twilio\Rest\Api\V2010\Account\MessageList;
 
@@ -20,52 +19,17 @@ class SmsTest extends TestCase
      */
     public function testShouldSendAnSms()
     {
-        $mockRestaurantController = $this->createMock(RestaurantController::class);
-        $mockRestaurantController->method("show")->willReturn(
-            $this->successResponse(
-                factory(Restaurant::class)->make()
-            )
-        );
-
         $mockSmsClient = $this->createMock(Client::class);
         $mockSmsClient->messages = $this->createMock(MessageList::class);
         $mockSmsClient->messages->method("create")->willReturn([]);
 
-        $smsService = new SmsService($mockRestaurantController, $mockSmsClient);
+        $smsService = new SmsService($mockSmsClient, null, null);
 
         $result = $smsService->send(1, '+17609794553');
 
-        $this->assertJsonStringEqualsJsonString(
-            $result->content(),
-            $this->successResponse(array('status' => 'sent'))->content()
-        );
-    }
-
-    /**
-     * /sms/send [POST]
-     * 404
-     */
-    public function testShouldNotSendAnSmsWithInvalidRestaurant()
-    {
-        $mockRestaurantController = $this->createMock(RestaurantController::class);
-        $mockRestaurantController->method("show")->willThrowException(
-            new NotFoundHttpException()
-        );
-
-        $mockSmsClient = $this->createMock(Client::class);
-        $mockSmsClient->messages = $this->createMock(MessageList::class);
-        $mockSmsClient->messages->method("create")->willReturn([]);
-
-        $smsService = new SmsService($mockRestaurantController, $mockSmsClient);
-
-        $result = $smsService->send(51, '+17609794553');
-
-        $this->assertJsonStringEqualsJsonString(
-            $result->content(),
-            $this->errorResponse(
-                'Does not exist any instance of restaurant with the given id',
-                404
-            )->content()
+        $this->assertArrayNotHasKey(
+            'error',
+            json_decode($result->content(), true),
         );
     }
 }
