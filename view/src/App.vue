d355da9f648eefd80a43da1c463439de1b5b517e
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-15 14:13:09 -0300

    :feature: Adding failed messages table

--- view/src/App.vue
+++ view/src/App.vue
@@ -3,13 +3,46 @@
     <header>
       <h3>Take Away</h3>
     </header>
+    <hr/>
     <main>
       <div class='content'>
-        <h4>Failed messages in last 24 hours</h4>
-      </div>
-      <div class='content'>
-        <h4>Recent Messages</h4>
+        <h5>Failed messages in last 24 hours</h5>
         <b-table
+          class='message-table'
+          id='failedMessages'
+          sticky-header
+          striped
+          hover
+          borderless
+          head-variant='light'
+          :items="failedMessages"
+          :fields="fields"
+          :sort-by.sync="failedSmsSortBy"
+          :sort-desc.sync="failedSmsSortDesc"
+          :per-page="failedSmsPerPage"
+          :current-page="failedSmsCurrentPage"
+          responsive="sm"
+        ></b-table>
+
+        <b-pagination
+          pills
+          v-model="failedSmsCurrentPage"
+          size="sm"
+          align="center"
+          :total-rows="failedSmsRows"
+          :per-page="failedSmsPerPage"
+          aria-controls="failedMessages"
+          first-text="First"
+          prev-text="Prev"
+          next-text="Next"
+          last-text="Last"
+        ></b-pagination>
+      </div>
+      <hr/>
+      <div class='content'>
+        <h5>Recent Messages</h5>
+        <b-table
+          class='message-table'
           id='lastMessages'
           sticky-header
           striped
@@ -53,23 +86,35 @@ export default {
   name: 'MessageLog',
   data() {
     return {
-      sortBy: 'id',
-      sortDesc: true,
-      currentPage: 1,
-      perPage: 5,
-      rows: 50,
       fields: [
         { key: 'body', sortable: true },
         { key: 'status', sortable: true },
         { key: 'created_at', sortable: true }
       ],
+
+      // Failed messages table
+      failedSmsSortBy: 'id',
+      failedSmsSortDesc: true,
+      failedSmsCurrentPage: 1,
+      failedSmsPerPage: 5,
+      failedSmsRows: 50,
+      failedMessages: [],
+      getFailedMessages: 'http://localhost:8080/messages?take=50',
+
+      // All messages table
+      sortBy: 'id',
+      sortDesc: true,
+      currentPage: 1,
+      perPage: 5,
+      rows: 50,
       messages: [],
-      getMessages: 'http://localhost:8080/messages?take=50'
+      getMessages: 'http://localhost:8080/messages?take=50',
     };
   },
 
   created() {
     this.getAllMessages();
+    this.getAllFailedMessages();
   },
 
   methods: {
@@ -83,7 +128,18 @@ export default {
         .catch(error => {
           console.log(error);
         });
-    }
+    },
+    getAllFailedMessages() {
+      axios
+        .get(this.getFailedMessages)
+        .then(response => {
+          this.failedMessages = response.data.data;
+          this.failedSmsRows = this.failedMessages.length;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
   },
 
   components: {
@@ -94,5 +150,15 @@ export default {
 </script>
 
 <style lang='scss'>
+  .content {
+    padding-top: 20px;
+  }
 
+  .content .message-table {
+    margin-top: 10px;
+  }
+
+  h5 {
+    margin-left: 20px;
+  }
 </style>
