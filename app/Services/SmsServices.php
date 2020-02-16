16e607172d3c7d597628ab38d27a1e891875b09b
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-16 18:47:10 -0300

    :sparkles: Adding sendScheduledSms to the sms service

--- app/Services/SmsServices.php
+++ app/Services/SmsServices.php
@@ -9,6 +9,7 @@ use App\Message;
 use Exception;
 use Illuminate\Http\Response;
 use Twilio\Rest\Client;
+use Carbon\Carbon;
 
 class SmsService implements SmsServiceInterface
 {
@@ -49,17 +50,18 @@ class SmsService implements SmsServiceInterface
      * @param
      * @return
      */
-    public function send($restaurant_id, $phone_number)
+    public function send($restaurant_id, $phone_number, $type)
     {
         $restaurant = $this->restaurant->findOrFail($restaurant_id);
 
-        $body = $this->buildMessageBody($restaurant);
+        $body = $this->buildMessageBody($restaurant, $type);
 
         $this->message = $this->message->create([
             'body' => $body,
             'status' => 'error',
             'restaurant_id' => $restaurant_id,
             'phone_number' => $phone_number,
+            'type' => $type,
         ]);
 
         try {
@@ -81,5 +83,39 @@ class SmsService implements SmsServiceInterface
         $this->message->save();
 
         return $this->successResponse($this->message);
+    }
+
+    /**
+     *
+     *
+     * @param
+     * @return
+     */
+    public function sendScheduledSms() {
+        $messages = $this->message->
+            join('restaurants', 'messages.restaurant_id', '=', 'restaurants.id')->
+
+            whereRaw(
+                'DATE_ADD(messages.updated_at, INTERVAL restaurants.delivery_time SECOND) > '.
+                'DATE_SUB(CURRENT_TIME(), INTERVAL 15 MINUTE)'
+            )->
+
+            whereRaw(
+                'DATE_ADD(messages.updated_at, INTERVAL restaurants.delivery_time SECOND) < '.
+                'DATE_SUB(CURRENT_TIME(), INTERVAL 0 MINUTE)'
+            )->
+            where("type", "=", "before")->
+            where("status", "=","delivered")->
+            get();
+
+        foreach ($messages as $message) {
+            $this->send(
+                $message['restaurant_id'],
+                $message['phone_number'],
+                'after'
+            );
+        }
+
+        return sizeof($messages);
     }
 }
