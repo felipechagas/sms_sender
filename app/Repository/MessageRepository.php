7f192a95ad008b2340805b54a56ce8718604dfa0
Felipe Chagas Pereira <felip.uece@gmail.com>
2020-02-17 01:26:44 -0300

    :building_construction: Adding every messages logic to its repository

--- app/Repository/MessageRepository.php
+++ app/Repository/MessageRepository.php
@@ -5,19 +5,22 @@ namespace App\Repository;
 use App\Traits\ApiResponser;
 use App\Message;
 use Carbon\Carbon;
+use Illuminate\Http\Response;
 
 class MessageRepository implements MessageRepositoryInterface
 {
     use ApiResponser;
+
+    protected $messageModel;
 
     /**
      * Create a new MessageRepository instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Message $messageModel = null)
     {
-        //
+        $this->messageModel = $messageModel === null ? new Message() : $messageModel;
     }
 
     /**
@@ -28,26 +31,108 @@ class MessageRepository implements MessageRepositoryInterface
      */
     public function index($query = array())
     {
-        $messages = new Message();
+        $message = $this->messageModel;
 
         if (array_key_exists('status', $query)) {
-            $messages = $messages->where('status', '=', $query['status']);
+            $message = $message->where('status', '=', $query['status']);
         }
 
         if (array_key_exists('body', $query)) {
-            $messages = $messages->where('body', 'LIKE', '%' . $query['body'] . '%');
+            $message = $message->where('body', 'LIKE', '%' . $query['body'] . '%');
         }
 
         if (array_key_exists('from', $query)) {
-            $messages = $messages->
+            $message = $message->
                 where("updated_at", ">", Carbon::now()->subMinutes($query['from']));
         }
 
         if (array_key_exists('take', $query)) {
-            $messages = $messages->take($query['take'])->get();
+            $message = $message->take($query['take'])->get();
         } else {
-            $messages = $messages->get();
+            $message = $message->get();
         }
+
+        return $this->successResponse($message);
+    }
+
+    public function checkScheduledSms() {
+        $message = $this->messageModel;
+
+        return $message->
+            join('restaurants', 'messages.restaurant_id', '=', 'restaurants.id')->
+
+            whereRaw(
+                'DATE_ADD(messages.updated_at, INTERVAL restaurants.delivery_time SECOND) > '.
+                'DATE_SUB(CURRENT_TIME(), INTERVAL 91 MINUTE)'
+            )->
+
+            whereRaw(
+                'DATE_ADD(messages.updated_at, INTERVAL restaurants.delivery_time SECOND) < '.
+                'DATE_SUB(CURRENT_TIME(), INTERVAL 90 MINUTE)'
+            )->
+            where("type", "=", "before")->
+            where("status", "=","delivered")->
+            get();
+    }
+
+    /**
+     * Obtains and show one message
+     * @return Illuminate\Http\Response
+     */
+    public function findOrFail($message_id)
+    {
+        $messages = $this->messageModel;
+
+        $messages = $messages->findOrFail($message_id);
+
+        return $this->successResponse($messages);
+    }
+
+    /**
+     * Create one new message
+     * @return Illuminate\Http\Response
+     */
+    public function create($request)
+    {
+        $messages = $this->messageModel;
+
+        $messages = $messages->create($request);
+
+        return $this->successResponse($messages, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Update an existing message
+     * @return Illuminate\Http\Response
+     */
+    public function update($request, $message_id)
+    {
+        $messages = $this->messageModel;
+
+        $message = $messages->findOrFail($message_id);
+
+        $message->fill($request);
+
+        if ($message->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $message->save();
+
+        return $this->successResponse($message);
+    }
+
+    /**
+     * Remove an existing message
+     * @return Illuminate\Http\Response
+     */
+    public function delete($message_id)
+    {
+        $messages = $this->messageModel;
+
+        $messages = $messages->findOrFail($message_id);
+
+        $messages->delete();
 
         return $this->successResponse($messages);
     }
